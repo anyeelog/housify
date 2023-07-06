91626148ed3ee61e4600b794a9723b5c1f37cb1b
Angie Xu <anyixu98@gmail.com>
2023-07-06 17:47:23 +0200

    GET POST for creating a property

--- public/index.php
+++ public/index.php
@@ -9,7 +9,10 @@ $router = new Router();
 
 $router->get('/admin', [PropertyController::class, 'index']);
 $router->get('/properties/create', [PropertyController::class, 'create']);
+$router->post('/properties/create', [PropertyController::class, 'create']);
 $router->get('/properties/update', [PropertyController::class, 'update']);
+$router->post('/properties/update', [PropertyController::class, 'update']);
+$router->post('/properties/delete', [PropertyController::class, 'delete']);
 
 
 $router->testRoutes();
